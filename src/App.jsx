4d1e3f2514cccb38f65fbbd74973226482daf3c9
husein <softichusein8@gmail.com>
2024-07-10 01:41:37 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const StyledApp = styled.div`
+  padding: 20px;
+`;
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The wild oasis</H1>
+        <Button>Check in</Button>
+        <Button>Check out</Button>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
+}
+
+export default App;
